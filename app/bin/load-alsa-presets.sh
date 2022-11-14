284b86af7cf4735c66e00a6d28a2f68f4d0ac154
GioF_71 <giovanni.fulco@gmail.com>
2022-11-14 18:22:11 +0100

    implemented #52: support for alsa presets

---
+++ app/bin/load-alsa-presets.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+declare -A alsa_presets
+
+function load_preset_file() {
+    echo "Loading $2 alsa_presets..."
+    while IFS= read -r line
+    do
+        if [[ -n "$line" && ! $line = \#* ]]; then
+            key="$(cut -d '=' -f1 <<< ${line})"
+            keyLen=`echo ${#key}`
+            value=${line#*=} 
+            echo "Loading preset [$key]=[$value]"
+            alsa_presets[$key]=$value
+    fi
+    done < "$1"
+    echo "Finished loading $2 alsa_presets"
+}
+
+# load alsa_presets
+load_preset_file "/app/assets/alsa-presets.conf" "built-in"
+
+additional_alsa_presets_file="/user/config/additional-alsa-presets.conf"
+if [ -f $additional_alsa_presets_file ]; then
+    load_preset_file $additional_alsa_presets_file "additional"
+else
+    echo "No additional preset file found"
+fi
+
+sz=`echo "${#alsa_presets[@]}"`
+echo "There are [$sz] available alsa_presets"
+if [[ "${DISPLAY_alsa_presets}" == "Y" || "${DISPLAY_alsa_presets}" == "y" ]]; then  
+    for key in "${!alsa_presets[@]}"; do
+        echo "Preset ["$key"]=["${alsa_presets[$key]}"]"
+    done
+fi
