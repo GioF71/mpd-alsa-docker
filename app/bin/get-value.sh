0722afd5dc17e86b13bef73556977bbced0f2089
GioF_71 <giovanni.fulco@gmail.com>
2022-12-09 16:51:50 +0100

    [Feature] Allow an additional httpd output #110

--- app/bin/get-value.sh
+++ app/bin/get-value.sh
@@ -7,15 +7,13 @@ get_value() {
     if [ -z "${PRIO}" ]; then
         PRIO="file"
     fi
-    #echo "Searching value for [$ARG_NAME] Prio set to [$PRIO]"
-    #echo "Trying file first"
+    #Trying file first
     if [[ -v file_dict[${ARG_NAME}] ]]; then
         FROM_DICT=${file_dict[${ARG_NAME}]};
         #echo "FROM_DICT=[$FROM_DICT]"
     fi
-    #echo "Look in variables"
+    #Look in variables npw
     FROM_ENV=${!ARG_NAME}
-    #echo "$ARG_NAME available as variable -> [${FROM_ENV}]"    
     if [[ "${PRIO}" == "file" ]]; then
         if [[ -n "${FROM_DICT}" ]]; then
             RESULT=$FROM_DICT
@@ -29,6 +27,52 @@ get_value() {
             RESULT=$FROM_DICT
         fi
     fi
-    #echo "RESULT=[$RESULT]"
     echo ${RESULT}
 }
+
+get_named_env_name() {
+    VAR_NAME=$1
+    VAR_INDEX=$2
+    if [[ -z "$VAR_INDEX" || $VAR_INDEX -eq 0 ]]; then
+        #not appending index
+        COMPOSED_NAME=${VAR_NAME}
+    else
+        #appending index
+        COMPOSED_NAME=${VAR_NAME}_${VAR_INDEX}
+    fi
+    echo ${COMPOSED_NAME}
+}
+
+get_named_env() {
+    VAR_NAME=$1
+    VAR_INDEX=$2
+    SELECT_VAR=$(get_named_env_name $VAR_NAME $VAR_INDEX)
+    echo ${!SELECT_VAR}
+}
+
+get_indexed_default() {
+    DEF_VALUE=$1
+    VAR_INDEX=$2
+    if [[ -z "$VAR_INDEX" || $VAR_INDEX -eq 0 ]]; then
+        #not appending index
+        INDEXED_DEFAULT=${DEF_VALUE}
+    else
+        #appending index
+        INDEXED_DEFAULT=${DEF_VALUE}_${VAR_INDEX}
+    fi
+    echo ${INDEXED_DEFAULT}
+}
+
+get_indexed_default_num() {
+    DEF_VALUE=$1
+    VAR_INDEX=$2
+    if [[ -z "$VAR_INDEX" || "$VAR_INDEX" -eq 0 ]]; then
+        #return default
+        INDEXED_DEFAULT=${DEF_VALUE}
+    else
+        #sum to default
+        INDEXED_DEFAULT=$((${DEF_VALUE}+${VAR_INDEX}))
+    fi
+    echo ${INDEXED_DEFAULT}
+}
+
