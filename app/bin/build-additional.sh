fc2b5ac29b2822a565bb3006ac32dfbd3531984c
Giovanni Fulco <giovanni.fulco@gmail.com>
2022-12-10 12:41:52 +0100

    [Feature] Support mixer_type for httpd output #120 (#121)

--- app/bin/build-additional.sh
+++ app/bin/build-additional.sh
@@ -68,6 +68,7 @@ build_httpd() {
         add_output_parameter $out_file $idx HTTPD_OUTPUT_ALWAYS_ON always_on yes constant
         add_output_parameter $out_file $idx HTTPS_OUTPUT_TAGS tags yes constant
         add_output_parameter $out_file $idx HTTPD_OUTPUT_FORMAT format 44100:16:2 constant
+        add_output_parameter $out_file $idx HTTPD_OUTPUT_MIXER_TYPE mixer_type "" none
         close_output $out_file
     fi
 }
