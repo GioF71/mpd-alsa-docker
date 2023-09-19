ce98f4116f272d0b090be6b1aaa624f1989f56bf
Giovanni Fulco <giovanni.fulco@gmail.com>
2023-09-19 16:52:19 +0200

    [Feature] Extend coverage of proxy plugin #324

--- app/bin/run-mpd.sh
+++ app/bin/run-mpd.sh
@@ -300,7 +300,7 @@ if [ "${DATABASE_MODE^^}" == "SIMPLE" ]; then
     echo "  path \"/db/tag_cache\"" >> $MPD_ALSA_CONFIG_FILE
 elif [ "${DATABASE_MODE^^}" == "PROXY" ]; then
     echo "  host \"${DATABASE_PROXY_HOST}\"" >> $MPD_ALSA_CONFIG_FILE
-    echo "  port \"${DATABASE_PROXY_PASSWORD}\"" >> $MPD_ALSA_CONFIG_FILE
+    echo "  port \"${DATABASE_PROXY_PORT}\"" >> $MPD_ALSA_CONFIG_FILE
     if [[ -n "${DATABASE_PROXY_PASSWORD}" ]]; then
         echo "  password \"${DATABASE_PROXY_PASSWORD}\"" >> $MPD_ALSA_CONFIG_FILE
     fi
